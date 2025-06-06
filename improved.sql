a4b4be2720ca025fe4e46c68c7593ff4aaf4982c
matandabug <matan.baruch@mail.huji.ac.il>
2025-06-06 16:23:13 +0300

    asdasd

--- improved.sql
+++ improved.sql
@@ -1,5 +1,18 @@
-select distinct e1.year, e1.name, e1.state
-from elected_officials e1
-where e1.votes >= ALL (select e2.votes
-                        from elected_officials e2
-                        where e1.year = e2.year)
+explain analyze
+SELECT DISTINCT
+    e1.year,
+    e1.name,
+    e1.state
+FROM
+    elected_officials e1
+JOIN (
+    SELECT
+        year,
+        MAX(votes) AS max_votes
+    FROM
+        elected_officials
+    GROUP BY
+        year
+) AS e2 ON e1.year = e2.year AND e1.votes = e2.max_votes
+ORDER BY
+    e1.year;
---
+++ v.sql
@@ -0,0 +1,11 @@
+CREATE INDEX idx_elected_officials_year_votes ON elected_officials (votes);
+
+explain analyze
+select distinct e1.year, e1.name, e1.state
+from elected_officials e1
+where votes = (select max(votes)
+ from elected_officials e2
+ where e2.year = e1.year)
+order by year;
+
+DROP INDEX idx_elected_officials_year_votes;
