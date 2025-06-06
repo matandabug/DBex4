d910fd3735db1be4c6159929c6c05a4bb7ac337c
Matan Rafalovitz <matanraf@gmail.com>
2025-06-06 15:39:57 +0300

    added improved2

---
+++ improved2.sql
@@ -0,0 +1,17 @@
+SELECT DISTINCT
+    e1.year,
+    e1.name,
+    e1.state
+FROM
+    elected_officials e1
+JOIN (
+    SELECT
+        year,
+        MAX(votes) AS max_votes
+    FROM
+        elected_officials
+    GROUP BY
+        year
+) AS e2 ON e1.year = e2.year AND e1.votes = e2.max_votes
+ORDER BY
+    e1.year;
